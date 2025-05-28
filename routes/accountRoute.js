cc87b37bea01919b1b4bc5d962ccc350988db9e1
woodpecker09 <ortegaharold99@outlook.es>
2025-05-28 13:19:07 -0600

    Learning Activity 1 and 2 done

---
+++ routes/accountRoute.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const router = new express.Router() 
+const utilities = require("../utilities/index")
+const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+router.get("/login",utilities.handleErrors(accountController.buildLogin))
+
+router.get("/register",utilities.handleErrors(accountController.buildRegister))
+
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+module.exports = router
+
--- server.js
+++ server.js
@@ -9,10 +9,38 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const bodyParser = require("body-parser")
+const session = require("express-session")
+const pool = require('./database/')
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities")
+const accountRoute = require("./routes/accountRoute")
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -27,6 +55,8 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// Account routes
+app.use("/account", accountRoute)
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
